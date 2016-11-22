83ab9091768977af48248f20fb8a4ff39fd6988f
howardmann <howardmann27@gmail.com>
2016-11-22 19:10:23 +1100

    Changed host files

--- knexfile.js
+++ knexfile.js
@@ -5,15 +5,16 @@ module.exports = {
     connection: {
       host: "127.0.0.1",
       user: "root",
-      database: "auth",
+      database: "auth"
     }
   },
   production: {
     client: "mysql",
     connection: {
-      host: "production",
-      user: "production",
-      database: "auth",
+      host: "us-cdbr-iron-east-04.cleardb.net",
+      user: "bc6fa5bdca11f5",
+      password: "e6d9f10f",
+      database: "heroku_6ed701c24f9e605"
     }
   }
 }
